7e46a3df7554621f7fa4ad703d0f191749775bf9
Shivani Pansara <shivanipansara222@gmail.com>
2021-10-31 13:59:52 -0400

    Added test case

--- spec/controllers/api/posts_controller_spec.rb
+++ spec/controllers/api/posts_controller_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Api::PostsController, type: :controller do
   
-  describe '#index' do
-    it "returns an error if no tags passed" do 
-      get api_posts
-
-      expect(response.content_type).to eq("application/json; charset=utf-8")
-      expect(JSON.parse(response.status)).to eq (400)
+  describe 'GET #index' do
+    before do
+      get :index
     end
+
+    it "returns an error if no tags passed" do 
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response.body).to eq('{"error":"tags parameter is required"}')
+      expect(response.status).to eq(400)
+    end
+
   end
 end
